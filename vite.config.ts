36b1edb10b83eb737df458ff9a7b8a4f575a29ec
Mohammed Moola <moemoola3@gmail.com>
2026-05-08 15:46:50 +0200

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,23 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
     resolveId(id) {
       if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
+        const filename = id.replace('figma:asset/', '');
+        return path.resolve(__dirname, 'src/assets', filename);
       }
     },
-  }
+  };
 }
 
 export default defineConfig({
-  plugins: [
-    figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [figmaAssetResolver(), react(), tailwindcss()],
+  base: 'https://github.com/MoeMoola3/OnTime.git',
   resolve: {
     alias: {
       // Alias @ to the src directory
@@ -33,4 +27,4 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
-})
+});
